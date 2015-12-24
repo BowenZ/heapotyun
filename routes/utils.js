616d898798dd6e85462b7257f9a1837afd4488c1
BowenZ <zhaobowen123@126.com>
2015-12-24 14:45:14 +0800

    add lukuang

--- routes/utils.js
+++ routes/utils.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var request = require('request'),
 	cheerio = require('cheerio');
+var needle = require('needle');
 
 router.get('/kuaidi', function(req, res, next) {
 	var url = 'http://wap.kuaidi100.com/wap_result.jsp?rand=20120517&fromWeb=null' + '&id=' + req.query.id + '&postid=' + req.query.postid;
@@ -16,6 +17,38 @@ router.get('/kuaidi', function(req, res, next) {
 		}
 		res.send('error');
 	});
+});
+
+router.get('/lukuang/:city', function(req,res, next){
+	if(req.params.city == 'zhengzhou'){
+		var url = 'http://www.zhongbuauto.com/weixin/jiaojing/road/';
+		var result;
+		needle.get(url, function(error, response){
+			if(!error && response.statusCode == 200){
+				result = {
+					msg: 'success',
+					data: []
+				};
+				var $ = cheerio.load(response.body);
+				$('td>div').each(function(index, el) {
+					result.data.push({
+						time: $(el).find('>div').eq(1).text(),
+						roadCondition: $(el).find('>div').eq(2).text()
+					});
+				});
+			}else{
+				result = {
+					msg: 'error'
+				};
+			}
+			res.send(result);
+		});
+	}else{
+		res.send({
+			msg: 'error',
+			info: 'can not find data in this city'
+		});
+	}
 });
 
 module.exports = router;
